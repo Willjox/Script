6b4ce01a49f36485e2e95ccfa33dfdfc3557062d
Willjox <william.jox@protonmail.com>
2018-11-01 11:10:53 +0100

    rsakeygen pill

--- mousetoggle.sh
+++ mousetoggle.sh
@@ -1,7 +1,6 @@
 x=$(xinput list-props 11 | sed '2!d' | tr -d -c 0-9) #Skräp, läs status |ta ut rad 2| ta bort allt utom siffror 
 if  [ "1471" == "$x" ]; then
 	$(xinput disable 11)
-	i
 else 
 	xinput enable 11
 fi
--- nyckelgen.sh
+++ nyckelgen.sh
@@ -1,7 +1,27 @@
 echo "alias: "
 read alias
+echo "Lösenord till kryptering, endast enter för att generera en okrypterad nyckel"
+echo "lösen: "
+read pwd
+if [ -n $pwd]; then
+	unencrytedkey 
+ else
+	 encryptedkey
+fi
 
-openssl genrsa -out $alias.key -des3
-openssl req -new -key $alias.key  -extensions v3_req -out $alias.csr
-openssl x509 -in ./$alias.csr -out ./$alias.crt -req -CA ./server.crt -CAkey ./server.key -CAcreateserial -days 3650 -extfile /etc/ssl/openssl.cnf -extensions v3_req
-
+unencrytedkey() {
+	#Generera en privat nyckel med namnet "alias".key
+	openssl genrsa -out $alias.key -des3
+	#generera en certificate signing request till den privata nyckeln
+	openssl req -new -key $alias.key  -extensions v3_req -out $alias.csr
+	#skapa ett signerat certifkat med CA nyckeln  från csren
+	openssl x509 -in ./$alias.csr -out ./$alias.crt -req -CA ./server.crt -CAkey ./server.key -CAcreateserial -days 3650 -extfile /etc/ssl/openssl.cnf -extensions v3_req
+}
+encryptedkey() {
+	#Generera en privat nyckel med namnet "alias".key och lösen pwd
+	openssl genrsa -out $alias.key -des3 -passout $pwd
+	#generera en certificate signing request till den privata nyckeln
+	openssl req -new -key $alias.key  -extensions v3_req -out $alias.csr -passin $pwd
+	#skapa ett signerat certifkat med CA nyckeln  från csren
+	openssl x509 -in ./$alias.csr -out ./$alias.crt -req -CA ./server.crt -CAkey ./server.key -CAcreateserial -days 3650 -extfile /etc/ssl/openssl.cnf -extensions v3_req
+}
